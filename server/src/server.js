6d3b0bf9a321a6bc0fbee3872dd935753383f2ed
Ansh <bahetianshh@gmail.com>
2026-03-25 17:30:00 +0530

    feat: add socket.io for real-time updates

--- server/src/server.js
+++ server/src/server.js
@@ -1,7 +1,23 @@
+require('dotenv').config();
+const http = require('http');
 const app = require('./app');
+const connectDB = require('./config/db');
 
 const PORT = process.env.PORT || 5005;
 
-app.listen(PORT, () => {
-  console.log(`PeerFlow server running on port ${PORT}`);
-});
+const startServer = async () => {
+    // Connect to MongoDB
+    await connectDB();
+
+    const server = http.createServer(app);
+
+    // ---------- Socket.io will be attached here in Part 8 ----------
+    // const { Server } = require('socket.io');
+    // const io = new Server(server, { cors: { origin: process.env.CLIENT_URL } });
+
+    server.listen(PORT, () => {
+        console.log(`🚀 PeerFlow server running on port ${PORT}`);
+    });
+};
+
+startServer();
